95c2ddd8a895ef602c8b5ad04f3b4cd38aec7c59
John Elesho <eleshojohno@gmail.com>
2021-01-06 08:46:02 +0100

    All commit

---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Dogs from '../views/Dogs.vue'
+import Cats from '../views/Cats.vue'
+import Pet from '../views/Pet.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/cats',
+    name: 'cats',
+    component: Cats
+  },
+  {
+    path: '/pets/:id',
+    name: 'pet',
+    component: Pet
+  },
+  {
+    path: '/dogs',
+    name: 'dogs',
+    component: Dogs
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
